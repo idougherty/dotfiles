f860ed27b51d8cd4da8534c9dae1fb93f86f0915
Ian Dougherty <ianedougherty01@gmail.com>
2025-06-16 11:25:05 -0500

    neovim opts to nixvim

--- nixvim/config/default.nix
+++ nixvim/config/default.nix
@@ -4,4 +4,18 @@
         ./bufferline.nix 
         ./theme.nix
     ];
+
+    opts = {
+        # clipboard = 'unnamedplus'; -- use system keyboard for yank vim has clipboard is 0 tho
+        number = true;
+        relativenumber = true;
+        tabstop = 4;
+        softtabstop = 4;
+        shiftwidth = 4;
+        expandtab = true;
+        smartindent = true;
+        wrap = false;
+        incsearch = true;
+        termguicolors = true;
+    };
 }
